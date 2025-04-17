c1ca8a4614da165b0aa3bd922cc5695f9c3c9ba8
asilbek <asilbekmirolimov@gmail.com>
2025-04-17 16:14:43 +0500

    changse

--- apps/users/models.py
+++ apps/users/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     class Meta:
